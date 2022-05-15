76efbd6c92d64cd233711589d9a08578ad8c3b70
Hamza <boujahamza@gmail.com>
2022-05-15 20:18:30 +0100

    added a route to gateway

--- routers/fileHandlerRouter.js
+++ routers/fileHandlerRouter.js
@@ -6,7 +6,12 @@ const httpProxy = require('express-http-proxy');
 
 const fileHandlerProxy = httpProxy("http://localhost:4001");
 
+
 router.get("/", (req, res, next) => {
+    fileHandlerProxy(req, res, next);
+})
+
+router.post("/", (req, res, next) => {
     fileHandlerProxy(req, res, next);
 })
 
